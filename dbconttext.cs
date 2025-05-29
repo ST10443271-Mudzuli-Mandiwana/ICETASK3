63788d55e42e763c96c31c5c27ca297c77448b6d
Mudzuli Mandiwana <163293815+ST10443271-Mudzuli-Mandiwana@users.noreply.github.com>
2025-05-29 19:45:26 +0200

    added all files related to code

---
+++ dbconttext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace CLDV6211_ICETASK2
+{
+
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
+
